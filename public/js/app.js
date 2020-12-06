1ae60051ce5e8a8b97b6e0bb0b042e233955d19c
snehit <snehitbatra125@gmai.com>
2020-12-06 01:37:06 -0800

    app

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ const m=document.querySelector('#da')
 
 weatherform.addEventListener('submit' , (e)=>{ e.preventDefault()
 const lo=search.value 
-fetch('http://localhost:3000/weather?search='+ lo).then((response)=>{ response.json().then((data)=>{ if(data.error){message.textContent="oopsie"} else {message.textContent=data.location ; 
+fetch('/weather?search='+ lo).then((response)=>{ response.json().then((data)=>{ if(data.error){message.textContent="oopsie"} else {message.textContent=data.location ; 
 msg.textContent="the temperature in " +data.location+" is "+data.temperature+" degree celcius " ;m.textContent=" date and time is "+data.date } } ) } )
 
 } )
